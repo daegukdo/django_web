fbda89b3efabebfc73e46d368d2e4e3ba9b1f3d7
eornr <eornrsayclub@gmail.com>
2020-05-27 14:30:13 +0900

    add .gitignore edit result page path (view.py)

--- blog/urls.py
+++ blog/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     url(r'^method/', views.method, name='method'),
     url(r'^result/', views.result, name='result'),
     url(r'^conclusion/', views.conclusion, name='conclusion'),
-    url(r'^result/sb1/', views.sub1, name='sub1'),
-    url(r'^result/sb2/', views.sub2, name='sub2'),
-    url(r'^result/sb3/', views.sub3, name='sub3'),
-    url(r'^result/sb4/', views.sub4, name='sub4'),
 ]
