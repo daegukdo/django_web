53ef1d803610a8c7df0d99c5f37e90c01473205c
DaeGuk Do <eornrsayclub@gmail.com>
2020-06-15 20:45:10 +0900

    edit website to covid 19 ver.

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '26kgyj^yug(_cyff#utu8z48^n$$4o*%ngsp78zov2i+i%uxub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
